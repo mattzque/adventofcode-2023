6674fd305d937ddbe0b39c8f970ca46722607e7f
Matthias Hecker <mail@apoc.cc>
2023-12-02 07:24:07 +0100

    Day 2

---
+++ src/day2_part2.rs
@@ -0,0 +1,101 @@
+use std::{collections::HashMap, fs::read_to_string};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Color {
+    Red,
+    Blue,
+    Green,
+}
+
+impl Color {
+    fn from_string(string: &str) -> anyhow::Result<Self> {
+        match string {
+            "red" => Ok(Self::Red),
+            "blue" => Ok(Self::Blue),
+            "green" => Ok(Self::Green),
+            _ => Err(anyhow::anyhow!("Invalid color!")),
+        }
+    }
+}
+
+#[derive(Default, Debug)]
+struct Draw(HashMap<Color, usize>);
+
+#[derive(Default, Debug)]
+struct Game {
+    id: usize,
+    draws: Vec<Draw>,
+}
+
+fn parse_draw(draw: &str) -> anyhow::Result<Draw> {
+    let mut colors = HashMap::new();
+    for cube in draw.split(',') {
+        let (count, color) = cube
+            .trim()
+            .split_once(' ')
+            .ok_or(anyhow::anyhow!("Invalid Draw!"))?;
+        let count =
+            str::parse::<usize>(count).map_err(|_| anyhow::anyhow!("Invalid Draw Count!"))?;
+        let color = Color::from_string(color)?;
+        colors.insert(color, count);
+    }
+
+    Ok(Draw(colors))
+}
+
+fn parse_game(line: &str) -> anyhow::Result<Game> {
+    let (game, rest) = line
+        .split_once(": ")
+        .ok_or(anyhow::anyhow!("Invalid game!"))?;
+    let id = str::parse::<usize>(&game["Game ".len()..])
+        .map_err(|_| anyhow::anyhow!("Invalid Game Id!"))?;
+    let mut draws = Vec::new();
+
+    for draw in rest.split(';').map(parse_draw) {
+        match draw {
+            Ok(draw) => draws.push(draw),
+            Err(err) => return Err(err),
+        }
+    }
+
+    Ok(Game { id, draws })
+}
+
+fn parse_games_from_contents(contents: &str) -> anyhow::Result<Vec<Game>> {
+    let lines = contents.split_terminator('\n');
+    let mut games = Vec::new();
+
+    for game in lines.map(parse_game) {
+        match game {
+            Ok(game) => games.push(game),
+            Err(err) => return Err(err),
+        }
+    }
+
+    Ok(games)
+}
+
+fn get_max_count_for_game(game: &Game) -> HashMap<Color, usize> {
+    let Game { draws, .. } = game;
+    draws.iter().fold(HashMap::new(), |mut max, draw| {
+        let Draw(cubes) = draw;
+        for (color, count) in cubes.iter() {
+            if count > max.entry(*color).or_insert(0) {
+                max.insert(*color, *count);
+            }
+        }
+        max
+    })
+}
+
+fn main() {
+    let contents = read_to_string("day2-input.txt").expect("Invalid Input!");
+    let games = parse_games_from_contents(&contents).expect("Invalid Input!");
+    let sum_of_powers: usize = games
+        .iter()
+        .map(get_max_count_for_game)
+        .map(|max| max.values().copied().reduce(|a, b| a * b).unwrap())
+        .sum();
+
+    println!("powers: {:?}", sum_of_powers);
+}
